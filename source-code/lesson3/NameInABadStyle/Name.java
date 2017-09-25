a663c63def1cb6e62fa8d748f87f105eb7350d12
diamondman3 <MaximumJM@gmail.com>
2017-09-25 14:57:17 -0700

    Add files via upload

--- source-code/lesson3/NameInABadStyle/Name.java
+++ source-code/lesson3/NameInABadStyle/Name.java
@@ -8,12 +8,9 @@
    private String myLast;
    
    public Name (String first, String middle, String last) {
-      myFirst = first;
-      myMiddle = middle;
-      myLast = last;
-      fixFirst ( );
-      fixMiddle ( );
-      fixLast ( );
+      myFirst = fixCase(first);
+      myMiddle = fixCase(middle);
+      myLast = fixCase(last);
    }
    
    public String lastFirst ( ) {
@@ -24,21 +21,13 @@
       return myFirst + " " + myMiddle + " " + myLast;
    }
    
-   // bad style
-   private void fixFirst ( ) {
-      myFirst = myFirst.toLowerCase().trim();
-      myFirst = myFirst.substring(0,1).toUpperCase() + myFirst.substring(1);
+   private String fixCase (String namePart) {
+       namePart=namePart.toLowerCase().trim();
+       namePart=namePart.substring(0,1).toUpperCase() + namePart.substring(1);
+       return namePart;
    }
    
-   // bad style
-   private void fixMiddle ( ) {
-      myMiddle = myMiddle.toLowerCase().trim();
-      myMiddle = myMiddle.substring(0,1).toUpperCase() + myMiddle.substring(1);
-   }
-   
-   // bad style
-   private void fixLast ( ) {
-      myLast = myLast.toLowerCase().trim();
-      myLast = myLast.substring(0,1).toUpperCase() + myLast.substring(1);
+   public String initials() {
+       return myFirst.substring(0,1)+myMiddle.substring(0,1)+myLast.substring(0,1)+"";
    }
 }
